95b06756d1cdd67435a10a58bc69c69b8e756853
jkueck <jkueck@eos-uptrade.de>
2023-07-21 17:21:45 +0200

    initial commit, add all relevant files

---
+++ src/main/java/de/jkueck/VehicleLogbookServiceApplication.java
@@ -0,0 +1,13 @@
+package de.jkueck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleLogbookServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleLogbookServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/jkueck/VehicleLogbookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.jkueck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleLogbookServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
